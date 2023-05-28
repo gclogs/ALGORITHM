b6681aa95659e5fb6bdfabd08c53d9f0b364e440
GyeongChan <40288646+gclogs@users.noreply.github.com>
2023-05-28 13:50:34 +0000

    [Silver I] 1914번: 하노이탑

--- java 11/Silver/1914/Main.java
+++ java 11/Silver/1914/Main.java
@@ -28,9 +28,14 @@ public class Main {
     }
 
     private static void hanoi(int n, int from, int to, int via) {
+        //n : 원판의개수, start : 시작막대번호 mid : 징검다리 막대 번호 end : 도착지막대번호
         if(n == 1) {
             steps.add(new int[] {from, to});
         } else {
+            /* n개의 원판을 시작지에서 도착지로 옮기는 과정을 아래의 3가지 과정으로 쪼갤수 있음 */
+            //1번과정 : n-1개 원판을 시작막대에서 도착 막대를 거쳐 중간막대로
+            //2번과정 : 시작막대의 마지막 남은 1개 원판을 도착지 막대로
+            //3번과정 : 중간막대로 옮겼던 n-1개 원판을 다시 도착지 막대로 이동.
             hanoi(n-1, from, via, to);
             steps.add(new int[] {from, to});
             hanoi(n-1, via, to, from);
