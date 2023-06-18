217774bcc73c583210bb919d1aa1a37f4adc5f12
GyeongChan <40288646+gclogs@users.noreply.github.com>
2023-06-18 14:17:20 +0000

    [Silver V] 2941번: 크로아티아 알파벳

--- 백준/java 11/jh05013/Step6/Silver/!2941/Main.java
+++ 백준/java 11/jh05013/Step6/Silver/!2941/Main.java
@@ -5,44 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    String s = br.readLine();
-    int count = 0;
     
-    for(int i = 0; i < s.length(); i++) {
-      char ch = s.charAt(i);
-      
-      if(ch == 'c' && i < s.length() - 1) { // 만약 ch가 c라면?
-        if(s.charAt(i + 1) == '=' || s.charAt(i + 1) == '-') {  // 만약 ch 다음 문자가 '=' 또는 '-' 이라면?
-          i++;  // i + 1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1 증가
-        }
-      } else if(ch == 'd' && i < s.length() - 1) {
-        if(s.charAt(i + 1) == '-') { // "d-" 일 경우
-          i++;
-        } else if(s.charAt(i + 1) == 'z' && i < s.length() - 2) { // "dz="일 경우
-          if(s.charAt(i + 2) == '=') {
-            i += 2;
-          }
-        }
-      } else if(ch == 'l' && i < s.length() - 1) {
-          if(s.charAt(i + 1) == 'j') {  // lj 일 경우
-            i++;
-          }
-      } else if(ch == 'n' && i < s.length() - 1) {
-          if(s.charAt(i + 1) == 'j') {  // nj 일 경우
-            i++;
-          }
-      } else if(ch == 's' && i < s.length() - 1) {
-          if(s.charAt(i + 1) == '=') {  // s= 일 경우
-            i++;
-          }
-      } else if(ch == 'z' && i < s.length() - 1) {
-          if(s.charAt(i + 1) == '=') {  // z= 일 경우
-            i++;
-          }
-      }
-      count++;
+    String[] croatia = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+    
+    String word = br.readLine();
+
+    for(String s : croatia) {
+      word = word.replace(s, "*");
     }
-    System.out.println(count);
+    System.out.println(word.length());
   }
 }
