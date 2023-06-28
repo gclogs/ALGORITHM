35acb5714942445cf0bab54e1db56a47182f5d84
GyeongChan <40288646+gclogs@users.noreply.github.com>
2023-06-28 03:56:51 +0000

    [Silver V] 2563번: 색종이

--- 백준/java 11/jh05013/Step7/Bronze/2563/Main.java
+++ 백준/java 11/jh05013/Step7/Bronze/2563/Main.java
@@ -11,9 +11,9 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int total = 0;  // 색칠한 영역의 넓이
+        boolean[][] paper = new boolean[100][100];  // 도화지 (true, false) 으로 비교
 
         int num = Integer.parseInt(br.readLine());  // 색종이 개수
-        boolean[][] paper = new boolean[100][100];  // 도화지 (true, false) 으로 비교
         
         for(int i = 0; i < num; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,7 +22,7 @@ public class Main {
 
             for(int j = x; j < x+10; j++) { // (x,y)부터 (x+9,y+9) 까지의 영역을 하나씩 체크
                 for(int k = y; k < y+10; k++) {
-                    if(!paper[j][k]) {
+                    if(paper[j][k] == false) {
                         paper[j][k] = true;
                         total++;    // 총 넓이에 더해줌
                     }
