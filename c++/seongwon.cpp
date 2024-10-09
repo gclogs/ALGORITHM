46080b8a7e2e340e4eaa8fb8c0c8dc61b0cf74fe
GyeongChan <40288646+gclogs@users.noreply.github.com>
2024-10-10 03:29:06 +0900

    fix: All about draw functions.

--- c++/seongwon.cpp
+++ c++/seongwon.cpp
@@ -1,117 +1,129 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 class Point {
-    private:
-        double x, y;
-    public:
-        Point() : x(0.0), y(0.0) {}
-        ~Point() {
-            cout << "[객체가 소멸됩니다.]\n";
-        }
+private:
+    double x, y;
+public:
+    Point() : x(0.0), y(0.0) {}
+    Point(double x, double y) : x(x), y(y) {}
+    
+    ~Point() {
+        cout << "Point 객체가 소멸됩니다.\n";  // 필요 없으면 주석 처리
+    }
 
-        double get_x() {
-            return x;
-        }
-        double get_y() {
-            return y;
-        }
-        void set_x(double x) {
-            this->x = x;
-        }
-        void set_y(double y) {
-            this->y = y;
-        }
-        bool checkOverflowNumbers(const vector<int>& nums) {
-            for (int num : nums) {
-                if (num <= 0 || num > 100) {
-                    return false;
-                }
-            }
-            return true;
-        }
+    double get_x() const {
+        return x;
+    }
+    double get_y() const {
+        return y;
+    }
+    void set_x(double x) {
+        this->x = x;
+    }
+    void set_y(double y) {
+        this->y = y;
+    }
+
+    bool isValid() const {
+        return x >= 0 && x <= 100 && y >= 0 && y <= 100;
+    }
 };
 
 class Rectangle {
-    private:
-        Point top_left;
-        Point top_right;
-    public:
-        bool isTopLeft() {
-            return top_left.get_x() != 0 || top_left.get_y() != 0;
+private:
+    Point top_left;
+    Point bottom_right;
+
+public:
+    Rectangle(Point tl, Point br) : top_left(tl), bottom_right(br) {}
+
+    bool isValidRectangle() {
+        return top_left.isValid() && bottom_right.isValid();
+    }
+
+    void fullRectangle() const {
+        int width = bottom_right.get_x() - top_left.get_x();
+        int height = bottom_right.get_y() - top_left.get_y();
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                cout << "*";
+            }
+            cout << endl;
         }
-        bool isTopRight() {
-            return top_right.get_x() != 0 || top_right.get_y() != 0;
-        }
-        void fullRectangle() {
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
+    }
+
+    void outlineRectangle() const {
+        int width = bottom_right.get_x() - top_left.get_x();
+        int height = bottom_right.get_y() - top_left.get_y();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
                     cout << "*";
+                } else {
+                    cout << " ";
                 }
-                cout << endl;
             }
+            cout << endl;
         }
-        void roundRectangle(int x, int y) {
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < y; j++) {
-                    if (i == 0 || i == 4 || j == 0 || j == 4) {
-                        cout << "*";
-                    } else {
-                        cout << " ";
-                    }
+    }
+
+    void drawCorners() const {
+        int width = bottom_right.get_x() - top_left.get_x();
+        int height = bottom_right.get_y() - top_left.get_y();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i == 0 && j == 0) || (i == height - 1 && j == width - 1)) {
+                    cout << "*";
+                } else {
+                    cout << " ";
                 }
-                cout << endl;
             }
+            cout << endl;
         }
+    }
 };
 
 int main() {
-    Rectangle rect; // 객체 생성
-
-    vector<int> numbers;
-    int x1, y1, x2, y2;
-
+    double x1, y1, x2, y2;
     cout << "x1, y1, x2, y2 값을 입력하세요 (공백으로 구분): ";
     cin >> x1 >> y1 >> x2 >> y2;
 
-    numbers.push_back(x1);
-    numbers.push_back(y1);
-    numbers.push_back(x2);
-    numbers.push_back(y2);
+    Point top_left(x1, y1);
+    Point bottom_right(x2, y2);
 
-    try {
-        // 예외 발생: 범위를 벗어난 인덱스 접근
-        for (int i = 0; i <= numbers.size(); i++) {
-            cout << "numbers[" << i << "] = " << numbers.at(i) << endl;
-        }
-    } catch (const out_of_range& e) {
-        cout << "예외 발생: " << e.what() << " (out of bound)" << endl;
+    if (!top_left.isValid() || !bottom_right.isValid()) {
+        cout << "out of bound" << endl;
+        return 0;
     }
 
-    if (checkOverflowNumbers(numbers)) {
-        int menu;
-        while (true) {
-            cout << "메뉴: 1. 전체 사각형 2. 둘레 사각형 0. 종료\n";
-            cin >> menu;
+    Rectangle rect(top_left, bottom_right);
 
-            switch (menu) {
-                case 0:
-                    return 0;
-                case 1:
-                    rect.fullRectangle();
-                    break;
-                case 2:
-                    rect.roundRectangle();
-                    break;
-                case 3:
-                    rect.
-                default:
-                    cout << "잘못된 선택입니다.\n";
-                    break;
-            }
-        }   
+    int menu;
+    while (true) {
+        cout << "메뉴: 1. 전체 사각형 2. 둘레 사각형 3. 모서리 점 그리기 0. 종료\n";
+        cin >> menu;
+
+        switch (menu) {
+        case 0:
+            return 0;
+        case 1:
+            rect.fullRectangle();
+            break;
+        case 2:
+            rect.outlineRectangle();
+            break;
+        case 3:
+            rect.drawCorners();
+            break;
+        default:
+            cout << "잘못된 선택입니다.\n";
+            break;
+        }
     }
 
     return 0;
